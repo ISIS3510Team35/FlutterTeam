6e2780d5cee03a2b478af102fd179b6d46b3771a
Melissa <m.contrerasr@uniandes.edu.co>
2023-11-06 23:43:48 -0500

    add firebase init main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fud/firebase_options.dart';
 import 'package:fud/login.dart';
 import 'package:fud/appRouter.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:fud/services/firebase_services.dart';
 import 'package:fud/services/localStorage.dart';
-import 'firebase_options.dart';
 
 void main() async {
   DateTime appStartTime = DateTime.now();
 
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   var localStorage = LocalStorage();
   localStorage.init();
 
